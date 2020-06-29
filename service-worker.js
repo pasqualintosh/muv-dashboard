ed4e34ea92991390fdb75c15cb9435c24f653357
Pasquale Sacco <pasqsac@yahoo.it>
2020-06-29 12:59:51 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pasqualintosh/muv-dashboard/precache-manifest.b6f8e50a71574c87ed0ff0d9690d44a4.js"
+  "/muv-dashboard/precache-manifest.45d2c8a2c6feb90dbd677dabadb7bc21.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pasqualintosh/muv-dashboard/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/muv-dashboard/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
